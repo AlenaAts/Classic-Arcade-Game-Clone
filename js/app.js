5218584d3ede0455c45ab7b0e1a4cedb102e5dca
Alena <amongstthestones@gmail.com>
2018-06-08 22:12:47 +0300

    cleared comments

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 
-let Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/********************************* enemy class *********************************/
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Constructor function for enemies
+let Enemy = function(x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
@@ -14,15 +12,13 @@ let Enemy = function(x, y, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += this.speed * dt;
     if (this.x > 505) {
         this.x = -100;
         this.speed = Math.floor(Math.random() * 200) + 100;
     }
 
+    // collision detection
    if (player.x < this.x + 50 && 
         player.x + 50 > this.x &&
         player.y < this.y + 50 &&
@@ -37,19 +33,16 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+/******************************** player class ********************************/
 
-
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+// Constructor function for the player
 let Player = function(x, y) {
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-boy.png';
 };
 
+// The player reaches the water and gets back to start position
 Player.prototype.update = function(dt) {
     if (player.y < 50) {
         setTimeout(function() {
@@ -59,15 +52,15 @@ Player.prototype.update = function(dt) {
     }
 };
 
+// Draw the player on the screen, required method for game
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// The player's move control by keyboard
 Player.prototype.handleInput = function(key) {
     let stepX = 101;
     let stepY = 83;
-    // moving character?
-
     /*switch(key) {
         case "left" && this.x > 0:
             this.x -= 101;
@@ -94,25 +87,23 @@ Player.prototype.handleInput = function(key) {
     }
 };
 
+/******************************************************************************/
 
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+// three enemy objects
 const first = new Enemy(0, 61, 150);
 const second = new Enemy(0,144, 100);
 const third = new Enemy(0, 227, 200);
+
+// coordinates for the player
 const startX = 202;
 const startY = 395;
 
+// defining enemies and the player
 const allEnemies = [first, second, third];
 const player = new Player(startX, startY);
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     let allowedKeys = {
         37: 'left',
